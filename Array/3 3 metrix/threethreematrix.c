41a9128fcae3e53521d482f34abfe53fa4975a96
Shubham-yadav36 <shybham36@gmail.com>
2019-03-27 23:14:45 +0530

    Two dimentional array example matrix

---
+++ Array/3 3 metrix/threethreematrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void main(){
+    int A[3][3],B[3][3],C[3][3],i,j;
+    printf("enter the 9 element for the first martix : ");
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++)
+            scanf("%d",&A[i][j]);
+    }
+    printf("enter the 9 element for the second matrix : ");
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++)
+            scanf("%d",&B[i][j]);
+    }
+    for(i=0;i<=2;i++){
+        for(j=0;j<=2;j++){
+            C[i][j]=A[i][j]+B[i][j];
+            printf("%d  ",C[i][j]);
+        }
+        printf("\n");
+    }
+    getch();
+
+}
