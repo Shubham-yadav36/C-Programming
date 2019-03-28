762b19b481139b7a397fbed5b88aca38c5909483
Shubham-yadav36 <shybham36@gmail.com>
2019-03-28 23:32:00 +0530

    there are sum basic of the stirng and some advanced

---
+++ String/basic strings/Input and print/input.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main(){
+    char name[20];
+    printf("Enter you name : ");
+    gets(name);
+    printf(" Welcome in c programmin Mr. %s",name);
+    getch();
+
+
+}
---
+++ String/basic strings/simple/namestr.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+void main(){
+    char name[20]="Shubham yadav9";
+    printf("%s\n",name);
+    printf("the size of the string is : %d",sizeof(name));
+    getch();
+
+}
