5498d5d21b6cc731c7d256a9e7a1576c5e309014
Shubham-yadav36 <shybham36@gmail.com>
2019-04-16 18:46:45 +0530

    prepocesser

---
+++ prepoccesser/#if #elif #endif/#.c
@@ -0,0 +1,15 @@
+
+#if country==india
+    printf("india is country of indians");
+#elif country==nepal
+    printf("this is the small country");
+#else country==pakistaan
+    printf("this is the country of terrirists");
+#endif // country
+
+#ifdef country
+    printf("india is my country")
+#endif // country
+#ifndef country
+    Printf("i love my family");
+#endif // country
